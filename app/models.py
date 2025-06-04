cf525e29407f1b62c4fe82de9d3378d7106533a7
Gerson V. <121315492+GerDeveelop@users.noreply.github.com>
2025-06-03 22:24:31 -0500

    Delete app directory

--- app/models.py
+++
@@ -1,7 +0,0 @@
-from . import db
-# clase para la base de datos
-class Registro(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    marca = db.Column(db.String(100), nullable=False)
-    localidad = db.Column(db.String(100), nullable=False)
-    aspirante = db.Column(db.String(100), nullable=False)
